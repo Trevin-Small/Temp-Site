e40436348dd2ef867b0736778a33e12d4f548b2a
Trevin Small <trevincub03@gmail.com>
2022-09-06 02:21:17 -0400

    Added my face lol and random quote button

--- dist/main.js
+++ dist/main.js
@@ -1,15 +1,22 @@
-window.onload = () => {
+function randomMessage(json) {
   const NUM_QUOTES = 30;
   const quote_element = document.getElementById('random-quote');
-
   let message = "";
+
+  message = json[String(Math.floor(Math.random() * NUM_QUOTES))];
+
+  quote_element.innerHTML = message;
+}
+
+window.onload = () => {
 
   fetch('https://abby.trevinsmall.com/dist/quotes.json')
     .then((response) => response.json())
     .then((json) => {
-      //message = json[String(Math.floor(Math.random() * NUM_QUOTES))];
-      message = json["29"];
+      randomMessage(json);
 
-      quote_element.innerHTML = message;
+      document.getElementById('message-button').addEventListener('click', () => {
+        randomMessage(json);
+      });
     });
 }
