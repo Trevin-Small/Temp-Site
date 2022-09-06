f6428d1f422747c2694005ef36377435c7b225a4
Trevin Small <trevincub03@gmail.com>
2022-09-05 23:16:48 -0400

    Updated quotes again

--- dist/main.js
+++ dist/main.js
@@ -7,8 +7,8 @@ window.onload = () => {
   fetch('https://abby.trevinsmall.com/dist/quotes.json')
     .then((response) => response.json())
     .then((json) => {
-      message = json[String(Math.floor(Math.random() * NUM_QUOTES))];
-      //message = json["29"];
+      //message = json[String(Math.floor(Math.random() * NUM_QUOTES))];
+      message = json["29"];
 
       quote_element.innerHTML = message;
     });
