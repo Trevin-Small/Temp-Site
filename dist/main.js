68a2d0bd72a1f9a5e79f9f3e3757cb564d21df19
Trevin Small <trevincub03@gmail.com>
2022-09-06 02:33:52 -0400

    Added prev message button

--- dist/main.js
+++ dist/main.js
@@ -1,9 +1,20 @@
-function randomMessage(json) {
+let message_number = 0;
+let previous_message_number = 0;
+
+function randomMessage(json, prev_message) {
   const NUM_QUOTES = 30;
   const quote_element = document.getElementById('random-quote');
   let message = "";
 
-  message = json[String(Math.floor(Math.random() * NUM_QUOTES))];
+  if (!prev_message) {
+    previous_message_number = message_number;
+    message_number = Math.floor(Math.random() * NUM_QUOTES);
+    message = json[String(message_number)];
+  } else {
+    message = json[String(previous_message_number)];
+    message_number = previous_message_number;
+  }
+
 
   quote_element.innerHTML = message;
 }
@@ -16,7 +27,11 @@ window.onload = () => {
       randomMessage(json);
 
       document.getElementById('message-button').addEventListener('click', () => {
-        randomMessage(json);
+        randomMessage(json, false);
+      });
+
+      document.getElementById('prev-button').addEventListener('click', () => {
+        randomMessage(json, true);
       });
     });
 }
