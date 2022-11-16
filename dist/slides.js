40d4ee31ed63942dddf8cd75525a00b26b17b12c
Trevin Small <trevincub03@gmail.com>
2022-11-15 23:40:56 -0500

    Update slides.js

--- dist/slides.js
+++ dist/slides.js
@@ -1,11 +1,12 @@
 const baseURL = "./assets/Pics for site/";
+const numSlides = 25;
 let slideIndex = 0;
 
 function showSlides() {
   let slide = document.getElementById("image");
   slide.setAttribute("src", baseURL + slideIndex + "-min.jpg");
   slideIndex++;
-  if (slideIndex > slide.length) {slideIndex = 1}
+  if (slideIndex > numSlides) {slideIndex = 0}
   setTimeout(showSlides, 4000); // Change image every 2 seconds
 }
 
