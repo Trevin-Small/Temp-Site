a17ffeca9f96404ce29abe498d4360698bfaf046
Trevin Small <trevincub03@gmail.com>
2022-11-15 23:35:30 -0500

    Compressed images and simplified slides.js

--- dist/slides.js
+++ dist/slides.js
@@ -1,15 +1,11 @@
+const baseURL = "./assets/Pics for site/";
 let slideIndex = 0;
 
 function showSlides() {
-  let i;
-  let slides = document.getElementsByClassName("slides");
-
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
+  let slide = document.getElementById("image");
+  slide.setAttribute("src", baseURL + slideIndex + "-min.jpg");
   slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}
-  slides[slideIndex-1].style.display = "block";
+  if (slideIndex > slide.length) {slideIndex = 1}
   setTimeout(showSlides, 4000); // Change image every 2 seconds
 }
 
